1e7f6f4a652a822916450efa8a7e083d03c85c53
MSBarbieri <matheus6152@hotmail.com>
2020-01-04 00:41:28 -0300

    correct copy

---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+var ghpages = require('gh-pages');
+
+console.log('Starting github pages deploy...');
+ 
+ghpages.publish('public', {
+  branch: 'master',
+  repo: 'git@github.com/MattBarbieri/mattbarbieri.github.io.git',
+  user: {
+    name: 'MSBarbieri',
+    email: 'matheussouzabarbieri@gmail.com'
+  }
+}, function(err) { console.error('Error: ' + err); });
+
+console.log('Deploy finished.');
